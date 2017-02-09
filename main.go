dd089db4c2efe5006e073221e0ba2d332990f289
Alex Yocom-Piatt <ay-p@decred.org>
2017-02-09 15:44:37 -0500

    First pass of BlockVersions data analysis and template filling (#5)

--- main.go
+++ main.go
@@ -18,23 +18,47 @@ import (
 	"github.com/decred/dcrrpcclient"
 )
 
-type hardForkInfo struct {
-	BlockHeight              int64
-	BlockVersionWindowValues map[uint64]*blockVersions
-}
+// Settings for daemon
+var dcrdCertPath = ("/home/user/.dcrd/rpc.cert")
+var dcrdServer = "127.0.0.1:19109"
+var dcrdUser = "USER"
+var dcrdPass = "PASSWORD"
 
-type blockVersions struct {
-	VersionPercentages map[int32]int64
-}
-
+// Daemon Params to use
 var activeNetParams = &chaincfg.TestNetParams
 
+// Webserver settings
+var listeningPort = ":8000"
+
+// Overall Data structure given to the template to render
+type hardForkInfo struct {
+	BlockHeight              int64
+	BlockVersions            map[int32]*blockVersions
+	BlockVersionsHeights     []int64
+	BlockVersionWindowLength uint64
+	BlockVersionThreshold    int
+}
+
+// Contains a certain block version's count of blocks in the
+// rolling window (which has a length of activeNetParams.BlockUpgradeNumToCheck)
+type blockVersions struct {
+	RollingWindowLookBacks []int
+}
+
 var hardForkInformation = &hardForkInfo{}
+
+var funcMap = template.FuncMap{
+	"minus": minus,
+}
+
+func minus(a, b int) int {
+	return a - b
+}
 
 func demoPage(w http.ResponseWriter, r *http.Request) {
 
 	fp := filepath.Join("public/views", "design_sketch.html")
-	tmpl, err := template.New("home").ParseFiles(fp)
+	tmpl, err := template.New("home").Funcs(funcMap).ParseFiles(fp)
 	if err != nil {
 		panic(err)
 	}
@@ -50,24 +74,45 @@ func updateHardForkInformation(dcrdClient *dcrrpcclient.Client) {
 	if err != nil {
 		fmt.Println(err)
 	}
-	stakeVersionResults, err := dcrdClient.GetStakeVersions(hash.String(), int32(activeNetParams.BlockUpgradeNumToCheck*2))
+	// Request twice as many, so we can populate the rolling block version window's first
+	stakeVersionResults, err := dcrdClient.GetStakeVersions(hash.String(),
+		int32(activeNetParams.BlockUpgradeNumToCheck*2))
 	if err != nil {
 		fmt.Println(err)
 	}
-	blockVersionLookBack := make(map[uint64]*blockVersions, activeNetParams.BlockUpgradeNumToCheck)
 
-	for i := uint64(0); i < activeNetParams.BlockUpgradeNumToCheck; i++ {
-		currentBlockVersions := &blockVersions{}
-		currentBlockVersionPercentages := make(map[int32]int64)
-		for j := i; j < activeNetParams.BlockUpgradeNumToCheck+i; j++ {
-			currentBlockVersionPercentages[stakeVersionResults.StakeVersions[i].BlockVersion] += currentBlockVersionPercentages[stakeVersionResults.StakeVersions[i].BlockVersion] + int64(1)
-			//fmt.Println(i, j)
+	blockVersionsFound := make(map[int32]*blockVersions)
+	blockVersionsHeights := make([]int64, activeNetParams.BlockUpgradeNumToCheck)
+	elementNum := 0
+	for i := len(stakeVersionResults.StakeVersions)/2 - 1; i >= 0; i-- {
+		windowEnd := i + int(activeNetParams.BlockUpgradeNumToCheck)
+		blockVersionsHeights[elementNum] = stakeVersionResults.StakeVersions[i].Height
+		stakeVersionsWindow := stakeVersionResults.StakeVersions[i:windowEnd]
+
+		for _, stakeVersion := range stakeVersionsWindow {
+			_, ok := blockVersionsFound[stakeVersion.BlockVersion]
+			if !ok {
+				// Had not found this block version yet
+				blockVersionsFound[stakeVersion.BlockVersion] = &blockVersions{}
+				blockVersionsFound[stakeVersion.BlockVersion].RollingWindowLookBacks = make([]int, activeNetParams.BlockUpgradeNumToCheck)
+				// Need to populate "back" to fill in values for previously missed window
+				for k := 0; k < elementNum; k++ {
+					blockVersionsFound[stakeVersion.BlockVersion].RollingWindowLookBacks[k] = 0
+				}
+				blockVersionsFound[stakeVersion.BlockVersion].RollingWindowLookBacks[elementNum] = 1
+			} else {
+				// Already had that block version, so increment
+				blockVersionsFound[stakeVersion.BlockVersion].RollingWindowLookBacks[elementNum] =
+					blockVersionsFound[stakeVersion.BlockVersion].RollingWindowLookBacks[elementNum] + 1
+			}
 		}
-		currentBlockVersions.VersionPercentages = currentBlockVersionPercentages
-		blockVersionLookBack[i] = currentBlockVersions
+		elementNum++
 	}
-	hardForkInformation.BlockVersionWindowValues = blockVersionLookBack
+	hardForkInformation.BlockVersionsHeights = blockVersionsHeights
+	hardForkInformation.BlockVersions = blockVersionsFound
 	hardForkInformation.BlockHeight = height
+	hardForkInformation.BlockVersionThreshold = int(float64(activeNetParams.BlockEnforceNumRequired) / float64(activeNetParams.BlockUpgradeNumToCheck) * 100)
+	hardForkInformation.BlockVersionWindowLength = activeNetParams.BlockUpgradeNumToCheck
 }
 
 var mux map[string]func(http.ResponseWriter, *http.Request)
@@ -91,16 +136,12 @@ func main() {
 		},
 	}
 	var dcrdCerts []byte
-	dcrdCertPath := ("/home/user/.dcrd/rpc.cert")
 	dcrdCerts, err := ioutil.ReadFile(dcrdCertPath)
 	if err != nil {
 		fmt.Printf("Failed to read dcrd cert file at %s: %s\n", dcrdCertPath,
 			err.Error())
 		os.Exit(1)
 	}
-	dcrdServer := "127.0.0.1:19109"
-	dcrdUser := "USER"
-	dcrdPass := "PASSWORD"
 	fmt.Printf("Attempting to connect to dcrd RPC %s as user %s "+
 		"using certificate located in %s\n",
 		dcrdServer, dcrdUser, dcrdCertPath)
@@ -144,7 +185,7 @@ func main() {
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("public/css/"))))
 	http.Handle("/fonts/", http.StripPrefix("/fonts/", http.FileServer(http.Dir("public/fonts/"))))
 	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("public/images/"))))
-	err = http.ListenAndServe(":8000", nil)
+	err = http.ListenAndServe(listeningPort, nil)
 	if err != nil {
 		fmt.Printf("Failed to bind http server: %s\n", err.Error())
 	}
