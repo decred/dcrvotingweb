699035da264acd9b503da424a8ba7a9063dd10be
karamble <hello@d3c.red>
2017-02-16 19:00:25 +0100

    Display Corrections of nested divs

--- tplhardforkdemo/src/js/main.js
+++ tplhardforkdemo/src/js/main.js
@@ -2,38 +2,38 @@ $('#showboxpow').click(function() {
     $('#boxpow').toggleClass('hidden');
     $('#boxpos').addClass('hidden');
     $('#boxstake').addClass('hidden');
-    $('.boxindicators').addClass('hidden');
+//    $('.boxindicators').addClass('hidden');
 });
 $('#showboxpos').click(function() {
     $('#boxpos').removeClass('hidden');
     $('#boxpow').addClass('hidden');
     $('#boxstake').addClass('hidden');
-    $('.boxindicators').addClass('hidden');
+//    $('.boxindicators').addClass('hidden');
 });
 $('#showboxstake').click(function() {
     $('#boxstake').toggleClass('hidden');
     $('#boxpos').addClass('hidden');
     $('#boxpow').addClass('hidden');
-    $('.boxindicators').addClass('hidden');
+//    $('.boxindicators').addClass('hidden');
 });
 
 $('.radial-progress').click(function() {
     $('#boxpow').toggleClass('hidden');
     $('#boxpos').addClass('hidden');
     $('#boxstake').addClass('hidden');
-    $('.boxindicators').addClass('hidden');
+//    $('.boxindicators').addClass('hidden');
 });
 
 $('.radial-progress2').click(function() {
      $('#boxpos').removeClass('hidden');
     $('#boxpow').addClass('hidden');
     $('#boxstake').addClass('hidden');
-    $('.boxindicators').addClass('hidden');
+//    $('.boxindicators').addClass('hidden');
 });
 
 $('.radial-progress3').click(function() {
     $('#boxstake').toggleClass('hidden');
     $('#boxpos').addClass('hidden');
     $('#boxpow').addClass('hidden');
-    $('.boxindicators').addClass('hidden');
+//    $('.boxindicators').addClass('hidden');
 });
