3afe804a709115c4eae14e465a06d517ea74ae06
karamble <hello@d3c.red>
2017-02-17 16:02:37 +0100

    Adding hidden containers and toggle functionality for detailed charts

--- tplhardforkdemo/src/js/main.js
+++ tplhardforkdemo/src/js/main.js
@@ -1,1 +1,15 @@
-
+$('.radial-progress').click(function() {
+$('#boxpow').toggleClass("hidden");
+$('#boxpos').addClass( "hidden" );
+$('#boxstake').addClass( "hidden" );
+});
+$('.radial-progress2').click(function() {
+$('#boxpos').toggleClass("hidden");
+$('#boxpow').addClass( "hidden" );
+$('#boxstake').addClass( "hidden" );
+});
+$('.radial-progress3').click(function() {
+$('#boxstake').toggleClass("hidden");
+$('#boxpos').addClass( "hidden" );
+$('#boxpow').addClass( "hidden" );
+});
