1fd543349d9e62b24a96d7693b0a6f892df0027f
chappjc <jonathan.chappelow@gmail.com>
2017-04-08 11:13:28 -0700

    Implement StakeVersions and GetStakeVersionInfo in StakeInfoStub.
    Add tests for these too.  Have tests go over multiple best height scenarios, just two currently.
    
    Fix typos.

--- nodeclientstub.go
+++ nodeclientstub.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"os"
+	"path/filepath"
 
 	"github.com/decred/dcrd/chaincfg/chainhash"
 	"github.com/decred/dcrd/dcrjson"
@@ -14,34 +16,65 @@ import (
 type StakeInfoStub struct {
 	status                  error
 	bestBlockHash           *chainhash.Hash
-	bestBlockHeigt          int64
+	bestBlockHeight         int64
 	bestBlockStakeVersion   uint32
 	blockVerboseResults     map[chainhash.Hash]*dcrjson.GetBlockVerboseResult
-	stakeVersionsResults    []*dcrjson.GetStakeVersionsResult
-	stakeVersionInfoResults []*dcrjson.GetStakeVersionInfoResult
+	stakeVersionsResults    *dcrjson.GetStakeVersionsResult
+	stakeVersionInfoResults *dcrjson.GetStakeVersionInfoResult
 	voteInfoResults         map[uint32]*dcrjson.GetVoteInfoResult
 }
 
 // NewStakeInfoStub is the constructor for StakeInfoStub. It reads JSON data
 // from strings or disk and unmarshals the data into the required dcrjson types
 // returned by the StakeInfoer methods.
-func NewStakeInfoStub() *StakeInfoStub {
-	voteInfoFake, err := ioutil.ReadFile("voteInfo4Fake.json")
+func NewStakeInfoStub(bestBlockOpt ...uint32) *StakeInfoStub {
+	bestBlock := uint32(289984)
+	if len(bestBlockOpt) > 0 {
+		bestBlock = bestBlockOpt[0]
+	}
+
+	stubPath := filepath.Join("stub_data", fmt.Sprintf("bestBlock%d", bestBlock))
+	_, err := os.Stat(stubPath)
+	if err != nil {
+		fmt.Printf("Unable to find stub path %s: %v", stubPath, err)
+	}
+
+	voteInfo4, err := ioutil.ReadFile(filepath.Join(stubPath, "getVoteInfo4.json"))
 	if err != nil {
 		panic(fmt.Sprint("Unable to read voteInfo4Fake.json: ", err))
 	}
-	voteInfoFakeResult := new(dcrjson.GetVoteInfoResult)
-	if err = json.Unmarshal(voteInfoFake, voteInfoFakeResult); err != nil {
+	voteInfo4Result := new(dcrjson.GetVoteInfoResult)
+	if err = json.Unmarshal(voteInfo4, voteInfo4Result); err != nil {
 		panic(fmt.Sprint("Unable to unmarshal voteInfo4Fake.json: ", err))
 	}
 
 	voteInfoResults := map[uint32]*dcrjson.GetVoteInfoResult{
-		voteInfoFakeResult.VoteVersion: voteInfoFakeResult,
+		voteInfo4Result.VoteVersion: voteInfo4Result,
 	}
 
-	bbHash, err := chainhash.NewHashFromStr(voteInfoFakeResult.Hash)
+	bbHash, err := chainhash.NewHashFromStr(voteInfo4Result.Hash)
 	if err != nil {
 		panic("invalid block hash in getvoteinfo results")
+	}
+
+	// getstakeversions
+	stakeVersions, err := ioutil.ReadFile(filepath.Join(stubPath, "getStakeVersions2016.json"))
+	if err != nil {
+		panic(fmt.Sprint("Unable to read getStakeVersions2016.json: ", err))
+	}
+	stakeVersionsResult := new(dcrjson.GetStakeVersionsResult)
+	if err = json.Unmarshal(stakeVersions, stakeVersionsResult); err != nil {
+		panic(fmt.Sprint("Unable to unmarshal getStakeVersions2016.json: ", err))
+	}
+
+	// getstakeversioninfo
+	stakeVersionInfo, err := ioutil.ReadFile(filepath.Join(stubPath, "getStakeVersionInfo144.json"))
+	if err != nil {
+		panic(fmt.Sprint("Unable to read getStakeVersionInfo144.json: ", err))
+	}
+	stakeVersionInfoResult := new(dcrjson.GetStakeVersionInfoResult)
+	if err = json.Unmarshal(stakeVersionInfo, stakeVersionInfoResult); err != nil {
+		panic(fmt.Sprint("Unable to unmarshal getStakeVersionInfo144.json: ", err))
 	}
 
 	// TODO: get the rest of the data
@@ -49,11 +82,11 @@ func NewStakeInfoStub() *StakeInfoStub {
 	stakeInfoStub := &StakeInfoStub{
 		status:                  nil,
 		bestBlockHash:           bbHash,
-		bestBlockHeigt:          289984,
-		bestBlockStakeVersion:   4,
+		bestBlockHeight:         int64(bestBlock),
+		bestBlockStakeVersion:   stakeVersionsResult.StakeVersions[0].StakeVersion,
 		blockVerboseResults:     make(map[chainhash.Hash]*dcrjson.GetBlockVerboseResult),
-		stakeVersionsResults:    nil,
-		stakeVersionInfoResults: nil,
+		stakeVersionsResults:    stakeVersionsResult,
+		stakeVersionInfoResults: stakeVersionInfoResult,
 		voteInfoResults:         voteInfoResults,
 	}
 
@@ -62,7 +95,7 @@ func NewStakeInfoStub() *StakeInfoStub {
 
 // GetBestBlock fakes dcrrpcclient's (*Client).GetBestBlock
 func (s *StakeInfoStub) GetBestBlock() (*chainhash.Hash, int64, error) {
-	return s.bestBlockHash, s.bestBlockHeigt, nil
+	return s.bestBlockHash, s.bestBlockHeight, nil
 }
 
 // GetBlockVerbose fakes dcrrpcclient's (*Client).GetBlockVerbose, but panics if
@@ -72,7 +105,7 @@ func (s *StakeInfoStub) GetBlockVerbose(blockHash *chainhash.Hash, verboseTx boo
 		return nil, fmt.Errorf("input block hash is not the best block hash of stub")
 	}
 	return &dcrjson.GetBlockVerboseResult{
-		Height:       s.bestBlockHeigt,
+		Height:       s.bestBlockHeight,
 		Hash:         s.bestBlockHash.String(),
 		StakeVersion: s.bestBlockStakeVersion,
 	}, nil
@@ -80,12 +113,32 @@ func (s *StakeInfoStub) GetBlockVerbose(blockHash *chainhash.Hash, verboseTx boo
 
 // GetStakeVersions fakes dcrrpcclient's (*Client).GetStakeVersions
 func (s *StakeInfoStub) GetStakeVersions(hash string, count int32) (*dcrjson.GetStakeVersionsResult, error) {
-	return nil, nil
+	if hash != s.bestBlockHash.String() {
+		return nil, fmt.Errorf("input block hash is not the best block hash of stub")
+	}
+	if int(count) > len(s.stakeVersionsResults.StakeVersions) {
+		return nil, fmt.Errorf("insufficient number of getstakeversions results")
+	}
+
+	stakeVersionsResult := new(dcrjson.GetStakeVersionsResult)
+	stakeVersionsResult.StakeVersions = s.stakeVersionsResults.StakeVersions[:count]
+
+	return stakeVersionsResult, nil
 }
 
 // GetStakeVersionInfo fakes dcrrpcclient's (*Client).GetStakeVersionInfo
 func (s *StakeInfoStub) GetStakeVersionInfo(count int32) (*dcrjson.GetStakeVersionInfoResult, error) {
-	return nil, nil
+	if int(count) > len(s.stakeVersionInfoResults.Intervals) {
+		return nil, fmt.Errorf("insufficient number of getstakeversioninfo intervals")
+	}
+
+	stakeVersionInfoResult := &dcrjson.GetStakeVersionInfoResult{
+		CurrentHeight: s.bestBlockHeight,
+		Hash:          s.bestBlockHash.String(),
+		Intervals:     s.stakeVersionInfoResults.Intervals[:count],
+	}
+
+	return stakeVersionInfoResult, nil
 }
 
 // GetVoteInfo fakes dcrrpcclient's (*Client).GetVoteInfo
--- nodeclientstub_test.go
+++ nodeclientstub_test.go
@@ -3,55 +3,112 @@ package main
 import "testing"
 
 func TestStakeInfoStub(t *testing.T) {
-	stub := NewStakeInfoStub()
-	if stub == nil {
-		t.Error("Unable to create stub")
+	bestHeightsExpected := []int64{273167, 289984}
+	bestHashesExpected := []string{"00000000062b8a5baa35ce53f274f29aeb57d77a4c15375c6f82c92ed1bea1e8",
+		"0000000000b0a130a86d7fe1e475b39da048c3215d958ea4b2e102ad5a687fc0"}
+
+	for ib, bestHeightExpected := range bestHeightsExpected {
+		bestHashExpected := bestHashesExpected[ib]
+
+		// StakeInfoStub constructor
+		stub := NewStakeInfoStub(uint32(bestHeightExpected))
+		if stub == nil {
+			t.Error("Unable to create stub")
+		}
+
+		// GetBestBlock
+		hash, height, err := stub.GetBestBlock()
+		if err != nil {
+			t.Error("GetBestBlock failed: ", err)
+		}
+
+		if hash.String() != bestHashExpected {
+			t.Errorf("Incorrect hash.  Got %s, expecting %s",
+				hash.String(), bestHashExpected)
+		}
+
+		if height != bestHeightExpected {
+			t.Errorf("Incorrect height.  Got %d, expecting %d",
+				height, bestHeightExpected)
+		}
+
+		// go test -v
+		t.Log("Hash: ", hash)
+		t.Log("Height: ", height)
+
+		// GetBlockVerbose
+		block, err := stub.GetBlockVerbose(hash, false)
+		if err != nil {
+			t.Errorf("Unable to get block %v: %v", hash, err)
+		}
+
+		if block.Height != bestHeightExpected {
+			t.Errorf("Got block with height %d, expected %d",
+				block.Height, bestHeightExpected)
+		}
+
+		// GetVoteInfo
+		voteInfo, err := stub.GetVoteInfo(4)
+		if err != nil {
+			t.Error("GetVoteInfo(4) failed: ", err)
+		}
+
+		if len(voteInfo.Agendas) != 1 {
+			t.Fatalf("Found %d agendas, expected %d", len(voteInfo.Agendas), 1)
+		}
+
+		choices := voteInfo.Agendas[0].Choices
+		if len(choices) != 3 {
+			t.Errorf("Found %d agenda choices, expected %d", len(choices), 3)
+		}
+
+		// GetStakeVersionInfo
+		numIntervals := 144
+		stakeVersionInfo, err := stub.GetStakeVersionInfo(int32(numIntervals))
+		if err != nil {
+			t.Errorf("GetStakeVersionInfo(%d) failed: %v", numIntervals, err)
+		}
+
+		if len(stakeVersionInfo.Intervals) != numIntervals {
+			t.Fatalf("Found %d stake version intervals, expected %d",
+				len(stakeVersionInfo.Intervals), numIntervals)
+		}
+
+		if stakeVersionInfo.CurrentHeight != bestHeightExpected {
+			t.Errorf("best block was %d, expected %d",
+				stakeVersionInfo.CurrentHeight, bestHeightExpected)
+		}
+
+		if stakeVersionInfo.Hash != bestHashExpected {
+			t.Errorf("Incorrect hash.  Got %s, expecting %s",
+				stakeVersionInfo.Hash, bestHashExpected)
+		}
+
+		// GetStakeVersions
+		numStakeVersions := 2016
+		stakeVersions, err := stub.GetStakeVersions(bestHashExpected, int32(numStakeVersions))
+		if err != nil {
+			t.Errorf("GetStakeVersions(%d) failed: %v", numStakeVersions, err)
+		}
+
+		if len(stakeVersions.StakeVersions) != numStakeVersions {
+			t.Fatalf("Found %d stake versions, expected %d",
+				len(stakeVersions.StakeVersions), numStakeVersions)
+		}
+
+		if stakeVersions.StakeVersions[0].Hash != bestHashExpected {
+			t.Errorf("Incorrect hash of first block.  Got %s, expecting %s",
+				stakeVersions.StakeVersions[0].Hash, bestHashExpected)
+		}
+
+		if stakeVersions.StakeVersions[0].Height != bestHeightExpected {
+			t.Errorf("first block height was %d, expected %d",
+				stakeVersions.StakeVersions[0].Height, bestHeightExpected)
+		}
+
+		if len(stakeVersions.StakeVersions[0].Votes) == 0 {
+			t.Error("votes is empty")
+		}
+
 	}
-
-	hash, height, err := stub.GetBestBlock()
-	if err != nil {
-		t.Error("GetBestBlock failed: ", err)
-	}
-
-	bestHashExpected := "0000000000b0a130a86d7fe1e475b39da048c3215d958ea4b2e102ad5a687fc0"
-	if hash.String() != bestHashExpected {
-		t.Errorf("Incorrect hash.  Got %s, expecting %s",
-			hash.String(), bestHashExpected)
-	}
-
-	bestHeightExpected := int64(289984)
-	if height != bestHeightExpected {
-		t.Errorf("Incorrect height.  Got %d, expecting %d",
-			height, bestHeightExpected)
-	}
-
-	// go test -v
-	t.Log("Hash: ", hash)
-	t.Log("Height: ", height)
-
-	block, err := stub.GetBlockVerbose(hash, false)
-	if err != nil {
-		t.Errorf("Unable to get block %v: %v", hash, err)
-	}
-
-	if block.Height != bestHeightExpected {
-		t.Errorf("Got block with height %d, expected %d",
-			block.Height, bestHeightExpected)
-	}
-
-	voteInfo, err := stub.GetVoteInfo(4)
-	if err != nil {
-		t.Error("GetVoteInfo(4) failed: ", err)
-	}
-
-	if len(voteInfo.Agendas) != 1 {
-		t.Fatalf("Found %d agendas, expected %d", len(voteInfo.Agendas), 1)
-	}
-
-	choices := voteInfo.Agendas[0].Choices
-	if len(choices) != 3 {
-		t.Errorf("Found %d agenda choices, expected %d", len(choices), 3)
-	}
-
-	// TODO: GetStakeVersions and GetStakeVersionInfo
 }
