1456d3c5c023520e5b026d5144763144ea39eb9b
sunburn <hello@d3c.red>
2017-04-28 23:44:40 +0200

    Chart updates, staked barcharts, pow + pos charts

--- public/js/hfsite.js
+++ public/js/hfsite.js
@@ -1,46 +1,5 @@
 $(document).ready(function() {
 
-
-
-	var time = 100,
-	    viewport = $(window),
-	    active = 'active',
-	    inactive = 'inactive',
-
-	    //agenda
-	    agenda = $('.agenda'),
-
-	    // agenda progressbar percent bar
-	    optionProgress = $('.option-progress'),
-	    totalVotesCast = $('.total-votes-cast'),
-
-	    // tooltip
-	    tooltip = $('.tooltip'),
-	    	tooltipDot = $('.tooltip-dot').hide(),
-	    	tooltipSquare = $('.tooltip-square').hide(),
-	    	tooltipOptionName = $('.tooltip-option-name'),
-	    	tooltipOptionValue = $('.tooltip-option-value'),
-
-	    // indicator icon
-	    indicatorIcon = $('.indicator-icon'),
-
-	    // charts desktop toggle button
-	    powPosChartToggle = $('.chart-toggle-side');
-
-	// charts draw
-	function drawTheChart(ChartData, ChartOptions, chartId, ChartType) {
-		var myChart = new Chart(document.getElementById(chartId).getContext('2d'),
-		    {
-		    	type: ChartType,
-		    	data: ChartData,
-		    	options: ChartOptions
-		    }
-		 );
-		document.getElementById(chartId).getContext('2d').stroke();
-	}
-
-
-
 	// tooltip
 	optionProgress.add(totalVotesCast).add(indicatorIcon).mouseover(function(e) {
 		var that = $(this);
