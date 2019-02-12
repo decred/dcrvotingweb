f6f0bdc018847e1d192bd39d316455ab943ba375
Jamie Holdstock <jam.jar69@gmail.com>
2019-02-12 13:43:16 +0000

    Remove jquery.

--- public/js/hfsite.js
+++ public/js/hfsite.js
@@ -1,37 +1,66 @@
-$(document).ready(function() {
+// tooltip
+var tooltip            = document.getElementById('tooltip');
+var tooltipDot         = document.getElementById('tooltip-dot');
+var tooltipOptionName  = document.getElementById('tooltip-option-name');
+var tooltipOptionValue = document.getElementById('tooltip-option-value');
 
-	// tooltip
-	optionProgress.add(totalVotesCast).add(indicatorIcon).mouseover(function(e) {
-		var that = $(this);
+// agenda progressbar percent bar
+var optionProgressBars = document.getElementsByClassName('option-progress');
 
-		that.on('mousemove', function(e) {
-		      tooltip.css({
-		         	left: e.pageX,
-		      	top: e.pageY + 25
-			});
-		});
+// indicator icon
+var indicatorIcons = document.getElementsByClassName('indicator-icon');
 
-		tooltipOptionName.text(that.attr('data-tooltip-text'));
-		tooltipOptionValue.text(that.attr('data-tooltip-value')+'%').show();
-		tooltipDot.css('background-color', that.css('background-color')).show();
+var hideToolTip = function() {
+	tooltip.style.display = 'none';
+}
 
-		if(that.is(optionProgress)) {
-			tooltipSquare.hide();
-			tooltipOptionName.removeClass('margin-right-5');
-		}
-		if(that.is(totalVotesCast)) {
-			tooltipDot.hide();
-			tooltipSquare.show();
-			tooltipOptionName.removeClass('margin-right-5');
-		}
-		if(that.is(indicatorIcon)) {
-			tooltipDot.add(tooltipSquare).add(tooltipOptionValue).hide();
-			tooltipOptionName.addClass('margin-right-5');
-		}
+var showProgressBarToolTip = function(event) {
+	var that = event.target;
 
-		tooltip.show();
-	}).mouseout(function() {
-		tooltip.hide();
+	that.addEventListener('mousemove', function (e) {
+		tooltip.style.left = e.pageX + "px";
+		tooltip.style.top = e.pageY + 25 + "px";
 	});
 
+	tooltipOptionName.textContent = that.getAttribute('data-tooltip-text');
+
+	tooltipDot.style.backgroundColor = that.style.backgroundColor;
+	tooltipDot.style.display = 'block';
+
+	tooltipOptionValue.textContent = that.getAttribute('data-tooltip-value') + '%';
+	tooltipOptionValue.style.display = 'block';
+
+	tooltipOptionName.classList.remove('margin-right-5');
+
+	tooltip.style.display = 'block';
+}
+
+var showIndicatorIconToolTip = function(event) {
+	var that = event.target;
+
+	that.addEventListener('mousemove', function(e) {
+		tooltip.style.left = e.pageX + "px";
+		tooltip.style.top = e.pageY + 25 + "px";
+	});
+
+	tooltipOptionName.textContent = that.getAttribute('data-tooltip-text');
+
+	tooltipDot.style.display = 'none';
+	tooltipOptionValue.style.display = 'none';
+
+	tooltipOptionName.classList.add('margin-right-5');
+
+	tooltip.style.display = 'block';
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+	for (var i = 0; i < indicatorIcons.length; i++) {
+		indicatorIcons[i].addEventListener("mouseover", showIndicatorIconToolTip);
+		indicatorIcons[i].addEventListener("mouseout", hideToolTip);
+	}
+
+	for (var i = 0; i < optionProgressBars.length; i++) {
+		optionProgressBars[i].addEventListener("mouseover", showProgressBarToolTip);
+		optionProgressBars[i].addEventListener("mouseout", hideToolTip);
+	}
 });
