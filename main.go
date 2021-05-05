3ab69263ef9e2e946e62c3ef33268949ff28561e
jholdstock <jholdstock@decred.org>
2021-05-05 17:10:43 +0100

    Add SIGTERM and SIGHUP to shutdown signals.

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 	"os/signal"
 	"strings"
 	"sync"
+	"syscall"
 	"time"
 
 	"github.com/decred/dcrd/rpcclient/v6"
@@ -362,7 +363,7 @@ func mainCore() int {
 
 	// Only accept a single CTRL+C
 	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM, syscall.SIGHUP)
 
 	// Start waiting for the interrupt signal
 	go func() {
