c69ef35479f652b4f1b2216e04dd674c3afe3fd0
Jamie Holdstock <jholdstock@decred.org>
2021-11-24 15:26:19 +0000

    build: Update to go 1.17. (#283)
    - Linter golint has been deprecated and replaced with revive.
    - GO111MODULE environment variable now defaults to "on"
    - Update README.md
    - Don't use deprecated ioutil package
    - Use new go:build tag syntax

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Build image
-FROM golang:1.15
+FROM golang:1.17
 
 LABEL description="dcrvotingweb build"
 LABEL version="1.0"
@@ -13,7 +13,7 @@ COPY ./ /root/
 RUN go build -v .
 
 # Serve image
-FROM golang:1.15
+FROM golang:1.17
 
 LABEL description="dcrvotingweb serve"
 LABEL version="1.0"
--- reloadsig.go
+++ reloadsig.go
@@ -1,3 +1,8 @@
+// Copyright (c) 2017-2021 The Decred developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+//go:build darwin || dragonfly || freebsd || linux || nacl || netbsd || openbsd || solaris
 // +build darwin dragonfly freebsd linux nacl netbsd openbsd solaris
 
 package main
